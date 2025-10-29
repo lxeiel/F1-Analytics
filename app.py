a0b66ec54ad9e49f159dea33f254e6f7028e99e3
seejaee <144614886+seejaee@users.noreply.github.com>
2025-10-29 23:35:05 +0800

    2025 season forecasting

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tabs.driverStats import driverStatsTab
 from tabs.overallStats import homeTab
 from tabs.constructorStats import constructorStatsTab
 from tabs.weatherAnalysis import weatherAnalysisTab
+from tabs.forecasting import forecastingTab
 
 st.set_page_config(
     page_title="F1 Dashboard",
@@ -13,7 +14,7 @@ st.set_page_config(
 
 st.title("🏁 F1 Dashboard")
 
-tabs = st.tabs(["Home", "Races", "Drivers", "Constructors", "Forecasting (?)", "Weather Analysis"])
+tabs = st.tabs(["Home", "Races", "Drivers", "Constructors", "Forecasting", "Weather Analysis"])
 with tabs[0]:
     homeTab()
 with tabs[1]:
@@ -23,8 +24,7 @@ with tabs[2]:
 with tabs[3]:
     constructorStatsTab()
 with tabs[4]:
-    st.write("do forecasting???")
-    st.link_button(label='reference link', url="https://www.kaggle.com/code/jalelgmiza1/f1-2025-season-analytics#%F0%9F%8F%8E%EF%B8%8F-PREPARE-2025-SEASON-FEATURES-AND-PREDICT-RACE-WINNERS")
+    forecastingTab()
 with tabs[5]:
     weatherAnalysisTab()
 
